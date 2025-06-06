62cc1e224529e51c0f0195a71ba91e1356b30a1c
Mohamed Ahmed <aero.mohamed@icloud.com>
2025-06-06 10:42:53 +0300

    interactive map is working but it needs more motion

---
+++ resources/js/Pages/TrafficMap/Index.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+import SEO from "@/Components/SEO.vue";
+import TrafficMap from "@/Pages/TrafficMap/Components/TrafficMap.vue";
+
+const props = defineProps({
+    page: Object,
+})
+</script>
+
+<template>
+    <BaseLayout>
+        <SEO :title="page?.title" :seo="page?.seo" />
+        <TrafficMap/>
+    </BaseLayout>
+</template>
+
+<style scoped>
+
+</style>
